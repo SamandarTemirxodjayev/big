c3dd8aa09b9d4edea0753d8bba5a09509c457dde
odilov-a <akbarjonodilov01@gmail.com>
2024-01-08 15:31:09 +0500

    #

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
 <template>
   <div>
+    <Head>
+      <Title>BIG METALL</Title>
+      <Meta name="description" content="Двухтавр, Швеллер, Угалок, Лист"/>
+      <Meta property="og:title" content="BIG METALL"/>
+      <Meta property="og:description" content="Двухтавр, Швеллер, Угалок, Лист"/>
+      <Meta property="og:url" content="https://bigmetall.uz/"/>
+      <Meta property="og:type" content="website"/>
+      <Meta name="keywords" content="Big Metall"/>
+      <Meta name="keywords" content="Metall"/>
+      <Meta name="keywords" content="Big"/>
+      <Meta name="keywords" content="Temir"/>
+      <Meta name="keywords" content="temir"/>
+      <Meta name="keywords" content="Двухтавр"/>
+      <Meta name="keywords" content="Швеллер"/>
+      <Meta name="keywords" content="Угалок"/>
+      <Meta name="keywords" content="Труба"/>
+      <Meta name="keywords" content="Лист"/>
+      <Meta name="keywords" content="Арматура"/>
+      <Meta name="keywords" content="Квадрат профил"/>
+      <Meta name="keywords" content="Квадрат прут"/>
+      <Meta name="keywords" content="Прут"/>
+      <Meta name="keywords" content="Металл паласа"/>
+      <Meta name="keywords" content="Металл планка"/>
+      <Meta name="rating" content="Family-Friendly"/>
+      <Meta name="yandex-verification" content="2d700806c890aa33" />
+      <Meta name="google-site-verification" content="PPevBp6aV4OJc1ZDkrMfYMlkI5LhICEpGPItHF3gWgA" />
+      <Link rel="stylesheet" href="./assets/css/main.css"/>
+      <Link rel="canonical" href="https://bigmetall.uz/"/>
+      <Link rel="icon" type="image/x-icon" href="./public/favicon.ico"/>
+    </Head>
     <div>
       <NuxtLayout>
         <NuxtPage />
@@ -26,5 +56,4 @@ body {
 }
 </style>
 <script setup>
-const colorMode = useColorMode();
 </script>
