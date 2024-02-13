ba6ed0e01d7725a274de0762aa7e125dd9541965
Shahkzodjon98 <shahkzodjon98@mail.ru>
2024-02-13 15:45:58 +0500

    endi bu oxirgisi bo'lishi kerak

--- components/Loader.vue
+++ components/Loader.vue
@@ -6,27 +6,76 @@
 </template>
 
 <style scoped>
-  .loader-container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-  }
-
- .loader {
-  width: 60px;
-  aspect-ratio: 4;
-  --c:#091E3A 100%,#0000;
-  background: 
-    radial-gradient(circle closest-side at left  60px top 50%,var(--c)),
-    radial-gradient(circle closest-side                      ,var(--c)),
-    radial-gradient(circle closest-side at right 60px top 50%,var(--c));
-  background-size: 100% 100%;
-  background-repeat: no-repeat;
-  animation: l4 1s infinite alternate;
+.loader-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 }
-@keyframes l4 {
-    to{width:100px;aspect-ratio: 1}
+
+.loader {
+  font-size: 10px;
+  margin: 50px auto;
+  text-indent: -9999em;
+  width: 11em;
+  height: 11em;
+  border-radius: 50%;
+  background: #09013a;
+  background: -moz-linear-gradient(left, #091e3a 10%, rgba(9, 1, 3, 0) 42%);
+  background: -webkit-linear-gradient(left, #091e3a 10%, rgba(9, 1, 3, 0) 42%);
+  background: -o-linear-gradient(left, #091e3a  10%, rgba(9, 1, 3, 0) 42%);
+  background: -ms-linear-gradient(left, #091e3a  10%, rgba(9, 1, 3, 0) 42%);
+  background: linear-gradient(to right, #091e3a 10%, rgba(9, 1, 3, 0) 42%);
+  position: relative;
+  -webkit-animation: load3 1.4s infinite linear;
+  animation: load3 1.4s infinite linear;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+}
+
+.loader:before {
+  width: 50%;
+  height: 50%;
+  background: #091e3a;
+  border-radius: 100% 0 0 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  content: '';
+}
+.loader:after {
+  background: #ffffff;
+  width: 75%;
+  height: 75%;
+  border-radius: 50%;
+  content: '';
+  margin: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+@-webkit-keyframes load3 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes load3 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
 }
 </style>
 
