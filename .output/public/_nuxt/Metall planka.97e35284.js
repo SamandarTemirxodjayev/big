832b845b57d9880055e12b3ab4e55cdf10f5ea4a
Samandar <iwtuz166@gmail.com>
2024-04-19 22:13:34 +0500

    build version

---
+++ .output/public/_nuxt/Metall planka.97e35284.js
@@ -0,0 +1,1 @@
+import"./entry.1e52db8c.js";const a=""+globalThis.__publicAssetsURL("icons/Metall palasa.svg"),l=""+globalThis.__publicAssetsURL("icons/Metall planka.svg");export{a as _,l as a};
---
+++ .output/public/_nuxt/default.59aeff54.js
@@ -0,0 +1,1 @@
+import{_ as r,a as _}from"./Footer.097663fc.js";import{_ as a,c,b as o,D as s,F as m,o as p}from"./entry.1e52db8c.js";import"./nuxt-link.ea2cd3e3.js";import"./Icon.d58d7b62.js";import"./index.b8fe2cb5.js";import"./index.8d1ae631.js";const l={};function f(t,d){const e=r,n=_;return p(),c(m,null,[o(e),s(t.$slots,"default"),o(n)],64)}const b=a(l,[["render",f]]);export{b as default};
